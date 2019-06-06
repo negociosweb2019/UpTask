88a1e42d7aa5816f7f26f5043cd7e0ff958f814b
Héctor Sabillón <h.hsabillon@gmail.com>
2019-06-06 14:54:37 -0600

    Inicializar la instancia de express y el script para iniciar el servidor.

---
+++ index.js
@@ -0,0 +1,8 @@
+// Importar los módulos de express
+const express = require('express');
+
+// Crear una App de express
+const app = express();
+
+// Inicializar el servidor de express en un puerto
+app.listen(7000);
