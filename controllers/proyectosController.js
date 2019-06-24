c6792e456afa9ce0aa2c689ecab84f4cff1613ef
Héctor Sabillón <h.hsabillon@gmail.com>
2019-06-24 14:45:10 -0600

    Creación de la validación de errores en el controlador ProyectosController para "Nuevo proyecto". Instalacion de las dependencias de servidor mysql2 y sequelize.

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -15,4 +15,21 @@ exports.nuevoProyecto = (req, res) => {
     // Utilizamos asignación por destructuring
     // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Operadores/Destructuring_assignment
     const { nombre } = req.body;
+    let errores = [];
+
+    // Verificar si el nombre del proyecto tiene un valor
+    if (!nombre) {
+        errores.push({'texto': 'El nombre del proyecto no puede ser vacío.'});
+    }
+
+    // Si hay errores
+    if (errores.length > 0) {
+        res.render('nuevoProyecto', {
+            nombrePagina : 'Nuevo proyecto',
+            errores
+        });
+    } else {
+        // No existen errores
+        // Inserción en la base de datos.
+    }
 };
